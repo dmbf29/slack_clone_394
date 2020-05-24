10d279644fcde3d9aeacfe513f9b8f95aa181bd6
Douglas Berkley <douglasmberkley@gmail.com>
2020-05-24 14:40:26 +0900

    added chatroom show and navbar user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def get_image_url(user)
+    if user.nickname == "douglas"
+      "https://kitt.lewagon.com/placeholder/users/dmbf29"
+    elsif user.nickname == "trouni"
+      "https://avatars3.githubusercontent.com/u/34345789?v=4"
+    else
+      "https://kitt.lewagon.com/placeholder/users/ssaunier"
+    end
+  end
 end
